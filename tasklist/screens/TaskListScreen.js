aab25128e116b3a41976ce98802770d6b0dadb3d
ainhoamayau <ainhoamaya.unir@gmail.com>
2024-11-19 20:15:15 +0100

    Se añade estilo a la vista de las tareas añadidas

--- tasklist/screens/TaskListScreen.js
+++ tasklist/screens/TaskListScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 
@@ -24,17 +24,40 @@ export default function TaskListScreen() {
   }, []);
 
   return (
-    <View>
-      <Text>Lista de Tareas</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Tareas</Text>
       <FlatList
         data={tasks}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.name}</Text>
+          <View style={styles.taskContainer}>
+            <Text style={styles.taskName}>{item.name}</Text>
           </View>
         )}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  taskContainer: {
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: '#66BB6A',
+    borderRadius: 5,
+  },
+  taskName: {
+    fontSize: 18,
+    color: 'white',
+  },
+});
+
