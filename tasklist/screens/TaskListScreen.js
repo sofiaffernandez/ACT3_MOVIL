3db1790d968c3257ad7e65752bf9a2c04cc94de9
ainhoamayau <ainhoamaya.unir@gmail.com>
2024-11-19 20:01:53 +0100

    Se crea la funcionalidad para ver las tareas añadidas

--- tasklist/screens/TaskListScreen.js
+++ tasklist/screens/TaskListScreen.js
@@ -1,13 +1,40 @@
-import React from'react';
-import { View, Text } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function TaskListScreen() {
+  const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await getDocs(collection(db, 'tasks'));
+        const tasksArray = response.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setTasks(tasksArray);
+      } catch (error) {
+        console.error('Error al obtener las tareas:', error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
 
   return (
     <View>
-      <Text> VENTANA DE LISTA</Text>
+      <Text>Lista de Tareas</Text>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
