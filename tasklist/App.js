02ef24b72690f8bd0a634382d6e9e17cb79e8567
Sofia Fernández <sofiaffeijoo98@gmail.com>
2024-11-21 11:06:39 +0100

    se añaden iconos al menu

--- tasklist/App.js
+++ tasklist/App.js
@@ -3,17 +3,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddTaskScreen from './screens/AddTaskScreen';
 import TaskListScreen from './screens/TaskListScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons'; 
 
 const Tab = createBottomTabNavigator();
 
+const icons = {
+  'Agregar tarea': 'format-list-bulleted-add',
+  'Ver tareas': 'list',
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => (
+            <Icon name={icons[route.name]} size={size} color={color} />
+          ),
+          tabBarActiveTintColor: '#77a863',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
         <Tab.Screen name="Agregar tarea" component={AddTaskScreen} />
         <Tab.Screen name="Ver tareas" component={TaskListScreen} />
-    </Tab.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
